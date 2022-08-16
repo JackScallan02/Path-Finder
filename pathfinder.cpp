b548915a9a043b21f15023321c7b8cc868a60a6a
Jack <jack.gregory.scallan@gmail.com>
2022-08-15 22:16:39 -0400

    Finished Maze class

--- pathfinder.cpp
+++ pathfinder.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
+#include <random>
 
 class Maze {
   int numRows;
   int numCols;
   std::vector<std::vector<char> > mazeRepresentation;
-
 
   public: Maze(int numRows, int numCols) {
     if (numRows < 4) { numRows = 4; }
@@ -17,69 +18,142 @@ class Maze {
   }
 
 
-  void constructMaze() {
-    /* IDEA
-      Initialize 2D array row*cols with 'X'
-      For first and last row and col, make 2 entrances of ' '
-      For all other rows and cols, randomly place ' ' (50/50 odds)
-      Set this->maze
-    */
-    /* PSUEDOCODE
-      1. Populate array 'mazeRepresentation' with 'X'
-      For (i to numRows)
-        For (j to numCols)
-          mazeRepresentation[i].push_back('X')
-
-      2. Create 2 entrances per side
-         For each column value in maze[0]
-            entrance1 = randomInt(0, numCols)
-            maze[entrance1] = ' '
-            entrance2 = randomChoice(randomInt(0, entrance1), (entrance1 + 1, numCols))
-            maze[entrance2] = ' '
-        For each column value in maze[numRows]...
-        For each row value in maze[i][numCols]...
-        For each row value in maze[i][0]...
-
-      3. Populate inner maze
-         For (i to numRows)
-            For (j to numCols)
-                maze[i][j] = randomChoice('X', ' ')
-
-      4. Set this->maze = maze
-    */
-
-    for (int i = 0; i < this->numRows; i++) {
-      std::vector<char> row;
-      this->mazeRepresentation.push_back(row);
-      for (int j = 0; j < this->numCols; j++) {
-        this->mazeRepresentation[i].push_back('X');
-      }
+  bool isBorder(int i, int j, int bottom, int right) {
+    //Returns true or false if coordinate is at a border of the maze
+    if (i == 0 || j == 0 || i == bottom || j == right) {
+      return true;
     }
+    return false;
+  }
 
+
+  int getRandomInt(int start, int end) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(start, end + 1);
+    return distr(gen);
+  }
+
+  void setBorderEntrances() {
+    //Sets a proportion border items ('X') to entrances (' ')
+    std::unordered_set<int> borderEntrances;
+
+    //Left Border
+    int randomIndex = getRandomInt(2, numRows - 2);
+    while (borderEntrances.size() < (numCols / 3)) {
+      if (borderEntrances.find(randomIndex) == borderEntrances.end()) {
+        borderEntrances.insert(randomIndex);
+        mazeRepresentation[randomIndex][1] = ' ';
+      }
+      randomIndex = getRandomInt(2, numRows - 2);
+    }
+    borderEntrances.clear();
+
+    //Right Border
+    randomIndex = getRandomInt(2, numRows - 3);
+    while (borderEntrances.size() < (numCols / 3)) {
+      if (borderEntrances.find(randomIndex) == borderEntrances.end()) {
+        borderEntrances.insert(randomIndex);
+        mazeRepresentation[randomIndex][numCols] = ' ';
+      }
+      randomIndex = getRandomInt(2, numRows - 2);
+    }
+    borderEntrances.clear();
+
+    //Top Border
+    randomIndex = getRandomInt(2, numCols - 2);
+    while (borderEntrances.size() < (numRows / 3)) {
+      if (borderEntrances.find(randomIndex) == borderEntrances.end()) {
+        borderEntrances.insert(randomIndex);
+        mazeRepresentation[1][randomIndex] = ' ';
+      }
+      randomIndex = getRandomInt(2, numCols - 2);
+    }
+    borderEntrances.clear();
+
+    //Bottom Border
+    randomIndex = getRandomInt(2, numCols - 2);
+    while (borderEntrances.size() < (numRows / 3)) {
+      if (borderEntrances.find(randomIndex) == borderEntrances.end()) {
+        borderEntrances.insert(randomIndex);
+        mazeRepresentation[numRows][randomIndex] = ' ';
+      }
+      randomIndex = getRandomInt(2, numCols - 2);
+    }
+    borderEntrances.clear();
 
   }
 
+  void setInnerPaths() {
+    //Sets a proportion of 'X' items in the maze to ' ' (open paths)
+    for (int i = 2; i < numRows - 1; i++) {
+      for (int j = 2; j < numCols - 1; j++) {
+        if (getRandomInt(0, 1)) {
+          mazeRepresentation[i][j] = ' ';
+        }
+      }
+    }
+  }
+
+
+  void constructMaze() {
+    //Constructs a maze with empty chars around the border and fills it
+    //with 'X' as a block or ' ' as an opening.
+
+    int rowIndex = 0;
+    std::vector<char> row;
+    mazeRepresentation.push_back(row);
+    while (rowIndex < numCols + 2) {
+      mazeRepresentation[0].push_back(' ');
+      rowIndex++;
+    }
+
+    for (int i = 1; i < numRows + 1; i++) {
+      row = std::vector<char>();
+      row.push_back(' ');
+      mazeRepresentation.push_back(row);
+      for (int j = 1; j < numCols + 1; j++) {
+        mazeRepresentation[i].push_back('X');
+      }
+      mazeRepresentation[i].push_back(' ');
+
+    }
+
+    rowIndex = 0;
+    row = std::vector<char>();
+    mazeRepresentation.push_back(row);
+    while (rowIndex < numCols + 2) {
+      mazeRepresentation[mazeRepresentation.size() - 1].push_back(' ');
+      rowIndex++;
+    }
+
+    setBorderEntrances();
+    setInnerPaths();
+  }
+
+
   int const getCols() {
-    return this->numCols;
+    return numCols;
   }
 
   int const getRows() {
-    return this->numRows;
+    return numRows;
   }
 
 
-
   std::vector<std::vector<char> > getMaze() {
-    return this->mazeRepresentation;
+    return mazeRepresentation;
   }
 
 };
 
+
 std::ostream& operator<<(std::ostream &s, Maze &maze) {
+  //Returns a string representation of the maze
   std::string output = "";
-  
-  for (int i = 0; i < maze.getRows(); i++) {
-    for (int j = 0; j < maze.getCols(); j++) {
+
+  for (int i = 0; i < maze.getRows() + 2; i++) {
+    for (int j = 0; j < maze.getCols() + 2; j++) {
       output += maze.getMaze()[i][j];
       output += " ";
     }
@@ -89,9 +163,9 @@ std::ostream& operator<<(std::ostream &s, Maze &maze) {
   return s << output;
 }
 
+
 int main() {
-  Maze maze(3, 4);
-  std::cout << maze;
+
   return 0;
 
 }
