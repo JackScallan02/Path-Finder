6603565b38c12d67f64fc10e9d1970f493afbcfe
Jack <jack.gregory.scallan@gmail.com>
2022-08-29 19:14:57 -0400

    Added test case and debugging function, modified algorithm outline

--- snake.cpp
+++ snake.cpp
@@ -18,29 +18,41 @@ std::vector<std::vector<int> > initialize2DVector(Maze maze) {
   return vect;
 }
 
-void backTrack(std::vector<std::vector<int> > &curPath, Maze &maze, int &backTrackAmount) {
+void backTrack(std::vector<int> &stack, Maze &maze) {
   //std::cout << "PATH: ";
-  std::cout << "Backtracking with an amount of " << backTrackAmount << std::endl;
-  sleep(1);
-  int pathSize = curPath.size();
-  for (int i = pathSize - 1; i >= pathSize - backTrackAmount; i--) {
 
-    maze.setMazeVal(curPath[i][0], curPath[i][1], ' ');
-    curPath.pop_back();
-    std::cout << maze << std::endl;
-    sleep(1);
-    std::cout << "Here" << std::endl;
+
+}
+
+Maze makeTestMaze() {
+  Maze testMaze(8, 8);
+  for (int row = 1; row < 9; row++) {
+    for (int col = 1; col < 9; col++) {
+      if (row == 4 && col < 5) {
+        testMaze.setMazeVal(row, col, ' ');
+      } else if ((row == 2 || row == 3 || row == 5 || row == 6) && col == 3) {
+        testMaze.setMazeVal(row, col, ' ');
+      } else {
+        testMaze.setMazeVal(row, col, 'X');
+      }
+    }
   }
+  return testMaze;
+}
 
+
+void printStack(std::vector<std::vector<int> > stack) {
+  int size = stack.size();
+  std::cout << "STACK: {";
+  for (int i = 0; i < size - 1; i++) {
+    std::cout << "(" << stack[i][0] << ", " << stack[i][1] << "), ";
+  }
+  std::cout << "(" << stack[size - 1][0] << ", " << stack[size - 1][1] << ")}" << std::endl;
 }
 
 
 
-void db(std::vector<int> curPos) {
-  std::cout << "Row index: " << curPos[0] << " Col index: " << curPos[1] << std::endl;
-}
-
-int snakeDfs(Maze maze, std::vector<int> curPos, int rightBound, int bottomBound, std::vector<std::vector<int> > &visited, std::vector<std::vector <int> > &curPosInfo) {
+int snakeDfs(Maze maze, std::vector<int> curPos, int rightBound, int bottomBound, std::vector<std::vector<int> > &visited) {
 /* Maze format
     0 1 2 3 4 5 6 7 8 9
   0
@@ -57,38 +69,30 @@ int snakeDfs(Maze maze, std::vector<int> curPos, int rightBound, int bottomBound
 
 
 /*
-Idea: When you first visit a node,
-    set curPosInfo[i][j] = number of nodes this node has yet to visit
+Algorithm:
+Keep entire path in stack
+If a dead end is found
+  pop current node from the stack
+  update the maze representation
 
-    If our node is at a deadend, we must backtrack
-      In backtrack function:
-          get last node in stack (current node)
-          pop it
-          Update maze
-          get last node in stack = LAST
-          if number of nodes that LAST has to visit > 0,
-              set curPosInfo[Last[0]][Last[1]] (num nodes to visit) -= 1
-              return (we will be dfsing it)
-          while (stack.size > 0 and LAST does not have any nodes to visit):
-            pop from stack
-            update maze
-            LAST = top of stack
-          set curPosInfo[Last[0]][Last[1]] (num nodes to visit) -= 1
 */
 
 
   std::vector<std::vector<char> > mazeRepresentation = maze.getMaze();
   std::vector<std::vector<int> > stack;
-  std::vector<std::vector<int> > curPath;
   stack.push_back(curPos);
+  int numberOfNeighbors;
 
   while (!stack.empty()) {
+    printStack(stack);
 
     curPos = stack.back();
     stack.pop_back();
-    curPath.push_back(curPos);
+
     maze.setMazeVal(curPos[0], curPos[1], 'O');
+
     visited[curPos[0]][curPos[1]] = 1;
+
 
     sleep(1);
     std::cout << maze << std::endl;
@@ -101,31 +105,32 @@ Idea: When you first visit a node,
     //left
     if (mazeRepresentation[curPos[0]][curPos[1] - 1] == ' ' && !visited[curPos[0]][curPos[1] - 1]) {
       std::vector<int> newPos = {curPos[0], curPos[1] - 1};
+      numberOfNeighbors++;
       stack.push_back(newPos);
     }
 
     //down
     if (mazeRepresentation[curPos[0] + 1][curPos[1]] == ' ' && !visited[curPos[0] + 1][curPos[1]]) {
       std::vector<int> newPos = {curPos[0] + 1, curPos[1]};
+      numberOfNeighbors++;
       stack.push_back(newPos);
     }
 
     //right
     if (mazeRepresentation[curPos[0]][curPos[1] + 1] == ' ' && !visited[curPos[0]][curPos[1] + 1]) {
       std::vector<int> newPos = {curPos[0], curPos[1] + 1};
+      numberOfNeighbors++;
       stack.push_back(newPos);
     }
 
     //up
     if (mazeRepresentation[curPos[0] - 1][curPos[1]] == ' ' && !visited[curPos[0] - 1][curPos[1]]) {
       std::vector<int> newPos = {curPos[0] - 1, curPos[1]};
+      numberOfNeighbors++;
       stack.push_back(newPos);
     }
 
-
-
   }
-
 
   return 0;
 
@@ -142,11 +147,11 @@ If it is 0, back track by the backtrackamount.
 
 
 int main() {
-  Maze maze(8, 8);
+  //Maze maze(8, 8);
+  Maze maze = makeTestMaze();
   std::cout << maze << std::endl;
   std::vector<int> curPos = {1, 0};
   std::vector<std::vector<int> > visited = initialize2DVector(maze);
-  std::vector<std::vector<int> > curPosInfo = initialize2DVector(maze); //Stores amount of paths not yet visited stemming from vertex[i][j]
-  snakeDfs(maze, curPos, maze.getRows(), maze.getCols(), visited, curPosInfo);
+  snakeDfs(maze, curPos, maze.getRows(), maze.getCols(), visited);
   return 0;
 }
